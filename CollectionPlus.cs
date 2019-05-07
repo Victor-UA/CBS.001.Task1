afdabc2894ea2fb76031f253d1d6bc0431ca5c18
Victor <Victor_UA@Ukr.net>
2019-05-07 19:04:50 +0300

    Add project files.

---
+++ CollectionPlus.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections;
+
+namespace Task01
+{
+    static class CollectionPlus
+    {                
+        public static IEnumerable Skip(this IEnumerable collection, int n)
+        {
+            object[] resultArray = new object[10];
+            int i = 0;            
+            foreach (var item in collection)
+            {
+                if (i >= n)
+                {
+                    ArrayAdd(ref resultArray, item, i - n);
+                }                
+                i++;
+            }
+
+            if (resultArray.Length > i - n)
+            {
+                ArrayResize(ref resultArray, i - n);
+            }
+
+            return resultArray;
+        }
+
+        public static IEnumerable Take(this IEnumerable collection, int n)
+        {
+            object[] resultArray = new object[n];
+            int i = 0;
+            foreach (var item in collection)
+            {
+                if (i < n)
+                {
+                    resultArray[i] = item;
+                }
+                else
+                {
+                    break;
+                }
+                i++;
+            }
+            if (resultArray.Length > i)
+            {
+                ArrayResize(ref resultArray, i);
+            }
+            return resultArray;
+        }
+
+        public static IEnumerable Distinct(this IEnumerable collection)
+        {
+            object[] resultArray = new object[1];
+            int i = 0;
+            foreach (var item in collection)
+            {
+                bool unique = true;
+                foreach (var arrayItem in resultArray)
+                {
+                    if (item.Equals(arrayItem))
+                    {
+                        unique = false;
+                    }
+                }
+                if (unique)
+                {
+                    ArrayAdd(ref resultArray, item, i++);
+                }
+            }
+            ArrayResize(ref resultArray, i);
+            return resultArray;
+        }
+
+        public static void PrintToConsole(this IEnumerable collection)
+        {
+            if (collection != null)
+            {
+                foreach (var item in collection)
+                {
+                    Console.WriteLine($"[{item}]");
+                }
+            }
+            else
+            {
+                Console.WriteLine("NULL");
+            }
+            Console.WriteLine();
+        }
+
+        private static void ArrayAdd(ref object[] source, object element, int index)
+        {
+            if (index >= source.Length)
+            {
+                int newLength = (int)(source.Length * 1.5 + 1);
+                ArrayResize(ref source, newLength);
+            }
+            source[index] = element;                
+        }
+
+        private static void ArrayResize(ref object[] source, int length, int indexFrom = 0)
+        {
+            if (length < 0)
+            {
+                source = new object[0];
+            }
+            else
+            {
+                object[] result = new object[length];
+                if (source.Length - indexFrom > result.Length)
+                {
+                    for (int i = 0; i < length; i++)
+                    {
+                        result[i] = source[i + indexFrom];
+                    }
+                }
+                else
+                {
+                    source.CopyTo(result, indexFrom);
+                }
+                source = result;
+            }
+        }
+
+        #region Array additions
+        private static object[] Add(this object[] source, object element, int index)
+        {
+            if (index > source.Length)
+            {
+                int newLength = (int)(source.Length * 1.5);
+                object[] resultArray = source.Resize(newLength);
+                resultArray[index] = element;
+                return resultArray;
+            }
+            else
+            {
+                source[index] = element;
+                return source;
+            }
+        }
+
+        private static object[] Resize(this object[] source, int length, int indexFrom = 0)
+        {
+            if (length < 0)
+            {
+                return new object[0];
+            }
+            object[] result = new object[length];
+            if (source.Length - indexFrom > result.Length)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    result[i] = source[i + indexFrom];
+                }
+            }
+            else
+            {
+                source.CopyTo(result, indexFrom);
+            }
+            return result;
+        }
+        #endregion        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable test = new List<int> { 1, 2, 3, 4, 5, 1, 2, 3, 4, 5 };
+
+            IEnumerable result = test.Skip(2);
+            result.PrintToConsole();
+
+            result = test.Take(4);
+            result.PrintToConsole();
+
+            result = test.Distinct();
+            result.PrintToConsole();
+
+            Console.ReadKey();
+        }
+    }
+}
